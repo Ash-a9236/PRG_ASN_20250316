c698752d88d5a09ab61b45cf47179d1701de30fd
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2025-03-16 15:07:40 -0400

    base methods for the class : Log class finished

--- src/main/java/asha9236/example/Log.java
+++ src/main/java/asha9236/example/Log.java
@@ -1,6 +1,10 @@
 package asha9236.example;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Objects;
 
 public class Log {
     private String id;
@@ -8,11 +12,67 @@ public class Log {
     private Date date;
     private String message;
 
-
-    public Log (String crewMember, Date date, String message) {
-        this.id = "L";
+//CONSTRUCTOR///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public Log (String crewMember, String message) {
+        this.id = "L" + dateFormatter();
         this.crewMember = crewMember;
-        this.date = date;
+        this.date = new Date();
         this.message = message;
+    }
+
+    public String dateFormatter () {
+        //since we are in space : need to specify the time zone
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/New_York"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd.HHmmssSSS");
+        String formattedDate = now.format(formatter);
+
+        return formattedDate;
+    }
+
+//BASE METHODS//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCrewMember() {
+        return crewMember;
+    }
+
+    public void setCrewMember(String crewMember) {
+        this.crewMember = crewMember;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Log log = (Log) o;
+        return Objects.equals(id, log.id) && Objects.equals(crewMember, log.crewMember)
+                && Objects.equals(date, log.date) && Objects.equals(message, log.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, crewMember, date, message);
+    }
+
+    @Override
+    public String toString() {
+        return "LOG " + id +
+                "\n    " + date +
+                "\n    Crew Member : " + crewMember +
+                "\n    Message : " + message;
     }
 }
