f7f85d8e41c833bf5d1c17822fa2ec8cbaa2a02f
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2025-03-16 15:02:23 -0400

    base methods for the class : Telemetry class finished

--- src/main/java/asha9236/example/Telemetry.java
+++ src/main/java/asha9236/example/Telemetry.java
@@ -1,16 +1,67 @@
 package asha9236.example;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Objects;
 
 public class Telemetry {
     private String id;
     private Date date;
     private String oxygenLevel;
 
-
-    public Telemetry (Date date, String oxygenLevel) {
-        this.id = "T";
-        this.date = date;
+//CONSTRUCTOR///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public Telemetry(String oxygenLevel) {
+        this.id = "T" + dateFormatter();
+        this.date = new Date();
         this.oxygenLevel = oxygenLevel;
+    }
+
+    public String dateFormatter() {
+        //since we are in space : need to specify the time zone
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/New_York"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd.HHmmssSSS");
+        String formattedDate = now.format(formatter);
+
+        return formattedDate;
+    }
+
+//BASE METHODS//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public String getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getOxygenLevel() {
+        return oxygenLevel;
+    }
+
+    public void setOxygenLevel(String oxygenLevel) {
+        this.oxygenLevel = oxygenLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Telemetry telemetry = (Telemetry) o;
+        return Objects.equals(id, telemetry.id) && Objects.equals(date, telemetry.date)
+                && Objects.equals(oxygenLevel, telemetry.oxygenLevel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, date, oxygenLevel);
+    }
+
+    @Override
+    public String toString() {
+        return "TELEMETRY " + id +
+                "\n    " + date +
+                "\n    Oxygen level : " + oxygenLevel;
     }
 }
